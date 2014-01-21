9dedd4803f7b369b4ec7f70570a6dea48ba9b04d
Chris Montes <chrismontes@about.me>
2014-01-20 22:54:38 -0800

    Implements rubocop suggestions

--- test/features/can_access_welcome_page_test.rb
+++ test/features/can_access_welcome_page_test.rb
@@ -1,9 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class CanAccessWelcomePageTest < Capybara::Rails::TestCase
-  test "It has a welcome headline" do
+  test 'It has a welcome headline' do
     visit root_path
-    assert_content page, "I make websites with my bear hands"
-    refute_content page, "Goobye All!"
+    assert_content page, 'I make websites with my bear hands'
+    refute_content page, 'Goobye All!'
   end
 end
