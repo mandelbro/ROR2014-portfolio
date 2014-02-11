d8b7d12b265e1a435742283465da0e9676299627
Chris Montes <chrismontes@about.me>
2014-02-11 03:44:47 -0800

    Adds projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,57 @@
+class ProjectsController < ApplicationController
+   before_filter :get_project, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project was successfully created."
+      redirect_to @project
+    else
+      # we'll get to this in a bit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @project.update_attributes(project_params)
+    if @project.save
+      flash[:notice] = "Project \"#{@project.company}\" was successfully updated."
+      redirect_to @project
+    else
+      # we'll get to this in a bit
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      flash[:notice] = "Project was successfully deleted."
+      redirect_to projects_path
+    else
+      # we'll get to this in a bit
+    end
+  end
+
+  private
+
+  def get_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:company, :body, :technologies, :lead_image, :other_images, :quote, :quote_attr)
+  end
+
+end
