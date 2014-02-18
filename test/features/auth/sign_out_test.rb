4dfd7a0fd106eb784b1e0b4bcdd30b15b941afd0
Chris Montes <chrismontes@about.me>
2014-02-18 01:09:23 -0800

    Adds sign in helper to sign out test

--- test/features/auth/sign_out_test.rb
+++ test/features/auth/sign_out_test.rb
@@ -5,11 +5,7 @@ feature 'As a user
          so that when I leave my information is secure' do
   scenario "user signs in to website, then signs out" do
     # Given I have signed into my account
-    visit "/"
-    page.find('#header').click_on "Sign In"
-    fill_in "Email", with: "rewt@test.com"
-    fill_in "Password", with: "password"
-    page.find("form").click_on "Sign in"
+    sign_in
 
     # When I click Sign out
     page.find('#header').click_on "Sign Out"
