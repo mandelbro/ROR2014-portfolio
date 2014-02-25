5095243252ed994ab00defcc1e88b528ac2b3568
Chris Montes <chrismontes@about.me>
2014-02-24 23:31:35 -0800

    Fixes config issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,11 @@ module ApplicationHelper
     content_for(:title, page_title.to_s)
   end
 
-end
+  def markdown text
+    options = { hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    raw markdown.render(text)
+  end
 
-def markdown text
-  options = { hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true }
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-  raw markdown.render(text)
+
 end
