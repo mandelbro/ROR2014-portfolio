128f700e74bab30198e8772d165754505a73459f
Chris Montes <chrismontes@about.me>
2014-03-02 17:48:26 -0800

    Adds tests for creating a post

--- test/features/posts/create_a_post_test.rb
+++ test/features/posts/create_a_post_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "Creating a Post" do
   scenario "submit form data to create a new post" do
     # Given a logged in user completed new post form
-    sign_in
+    sign_in(:author)
 
     visit new_post_path
     fill_in "Title", with: posts(:cr).title
@@ -16,6 +16,53 @@ feature "Creating a Post" do
     page.text.must_include "Post was successfully created"
     page.text.must_include posts(:cr).title
     page.has_css? "#author"
-    page.text.must_include users(:rewt).email # Use your fixture name here.
+    page.text.must_include users(:author).username # Use your fixture name here.
+    page.text.must_include "Status: Unpublished"
+    page.find('article.post').has_css? ".unpublished"
   end
+
+  scenario "unauthenticated site visitors cannot visit new_post_path" do
+    visit new_post_path
+    page.must_have_content "You need to sign in or sign up before continuing"
+  end
+
+  scenario "unauthenticated site visitors cannot see new post button" do
+    # When I visit the blog index page
+    visit posts_path
+    # Then I should not see the "New Post" button
+    page.wont_have_link "New Post"
+  end
+
+  scenario "authors can't publish" do
+    # Given an author's account
+    sign_in(:author)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is no checkbox for published
+    page.wont_have_field('Published')
+  end
+
+  scenario "editors can publish" do
+    # Given an editor's account
+    sign_in(:editor)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is a checkbox for published
+    page.must_have_field('Published')
+
+    # When I submit the form
+    fill_in "Title", with: posts(:cr).title
+    fill_in "Body", with: posts(:cr).body
+    check "Published"
+    click_on "Create Post"
+
+    # Then the published post should be shown
+    page.text.wont_include "Status: Unpublished"
+    page.find('article.post').has_css? ".published"
+  end
+
 end
