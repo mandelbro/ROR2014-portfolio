9cecc71964d067af4b8874b54c56d613f798c871
Chris Montes <chrismontes@about.me>
2014-03-04 02:29:27 -0800

    Implements authorize and policy scope in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,37 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = policy_scope(Post)
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    authorize @post
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+    authorize @post, :create?
   end
 
   # GET /posts/1/edit
   def edit
+    authorize @post, :update?
   end
 
   # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    authorize @post
     current_user.posts << @post
 
     respond_to do |format|
@@ -42,6 +48,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    authorize @post
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -56,9 +63,10 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    authorize @post
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to posts_url, notice: 'Post was successfully deleted.' }
       format.json { head :no_content }
     end
   end
@@ -71,6 +79,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :published)
+      params.require(:post).permit(*policy(@post || Post).permitted_attributes)
     end
 end
